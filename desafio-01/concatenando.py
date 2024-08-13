99d09072d1280d9ada0a500e22dec7d4a17f21c9
EnioFelipe <eniofelipe@alunos.utfpr.edu.br>
2024-08-13 20:17:36 -0300

    final

---
+++ desafio-01/concatenando.py
@@ -0,0 +1,9 @@
+#Este é um projeto simples, apenas para usar as funcionalidade do GitHub, o usuário deve informar 2 dados e o programa deve concatená-los em uma string.
+
+print("Realizando Cadastro:")
+
+nome = input("Digite aqui o seu nome:")
+idade = input("Digite aqui a sua idade:")
+
+print(f"Seu nome é {nome} e você tem {idade} anos")
+
